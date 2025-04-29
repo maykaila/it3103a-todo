ee260a8baa47583a3f703bbc41992369d825c264
duck <160460593+maykaila@users.noreply.github.com>
2025-04-29 13:39:53 +0800

    pull? pull (#30)
    * okay na to?
    
    * made cart and moved some things
    
    * draft 2 admin
    
    * db draft
    
    * php drafts [not yet tested]
    
    * idk dude
    
    ---------
    
    Co-authored-by: AlexaAko548 <160448789+AlexaAko548@users.noreply.github.com>

---
+++ IT3103A-PAINTING CHURVA/php/config.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$db   = 'chromatica';        // Your database name
+$user = 'root';    // Your database username
+$pass = '';    // Your database password
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+  $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+  die('Database Connection Failed: ' . $e->getMessage());
+}
+?>
