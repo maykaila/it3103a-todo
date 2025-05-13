85924df2cf2c23d799e8ce403c4be5eb8a83eb74
maykaila <23104019@usc.edu.ph>
2025-05-13 11:54:35 +0800

    removed to do list to move to another repo

--- TO-DO LIST/php/getUsername.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION['username'])) {
-    echo json_encode(["username" => $_SESSION['username']]);
-} else {
-    echo json_encode(["username" => "Guest"]); // Default username if not logged in
-}
-?>
